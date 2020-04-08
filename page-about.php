d44fcc63959532bd9301cee388a42daaf07c1090
EminQuna <eminquna96@gmail.com>
2020-04-08 10:45:32 +0200

    awesome theme

---
+++ page-about.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+	<?php 
+	
+	if( have_posts() ):
+		
+		while( have_posts() ): the_post(); ?>
+		
+        
+          <p><?php the_content(); ?></p>
+		   
+           <h3><?php the_title(); ?></h3>
+			
+			
+			<p><?php the_content(); ?></p>
+			
+			<hr>
+		
+		<?php endwhile;
+		
+	endif;
+			
+	?>
+
+<?php get_footer(); ?>
